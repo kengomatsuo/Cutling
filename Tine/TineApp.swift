de36ba9472d97f4914eb03140f813bef4d8038fd
Kenneth Johannes Fang <kennethfang1000@gmail.com>
2026-02-18 11:44:48 +0700

    Initial Commit

---
+++ Tine/TineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TineApp.swift
+//  Tine
+//
+//  Created by Kenneth Johannes Fang on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
